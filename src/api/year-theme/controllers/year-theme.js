b10974fce42682d1dabc490b75578541f4bcaf00
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2022-03-01 17:01:08 +0100

    added new collection types

---
+++ src/api/year-theme/controllers/year-theme.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  year-theme controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::year-theme.year-theme');
