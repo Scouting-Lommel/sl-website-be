1ea98ebfb2e10ae213b9e4fd637fdb8166bd3e80
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2022-03-02 16:51:11 +0100

    finnished structure

---
+++ src/api/general-data/routes/general-data.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * general-data router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::general-data.general-data');
