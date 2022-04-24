4383b0e325dd1c417bcd6159d5b3968135b21893
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2022-04-24 13:38:51 +0200

    added fields

---
+++ src/api/rented-date/controllers/rented-date.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  rented-date controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::rented-date.rented-date');
