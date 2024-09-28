efbafa6a49c69c17875c860ca1cafb3f68a87ad6
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-09-28 17:47:47 +0200

    Add salt tokens

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '36380e7c5164395199a1b6697bb40cf4'),
+    secret: env("ADMIN_JWT_SECRET"),
+  },
+  apiToken: {
+    salt: env("API_TOKEN_SALT"),
+  },
+  transfer: {
+    token: {
+      salt: env("TRANSFER_TOKEN_SALT"),
+    },
   },
 });
