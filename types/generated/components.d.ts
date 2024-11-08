9fcca683feea488623ca0a83971f952c70de904d
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-11-08 21:14:24 +0100

    Add map to map block

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -161,6 +161,8 @@ export interface ContentBlocksMapBlock extends Schema.Component {
   attributes: {
     title: Attribute.String;
     blockProperties: Attribute.Component<'general.block-properties'>;
+    location: Attribute.JSON &
+      Attribute.CustomField<'plugin::google-maps.location-picker'>;
   };
 }
 
