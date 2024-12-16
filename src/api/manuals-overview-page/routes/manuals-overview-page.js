c4feec42993df252001c6fed794a4e2ddd55f132
Seppe Alaerts <seppe.alaerts@craftzing.com>
2024-12-16 15:32:47 +0100

    Add manual and manual overview page

---
+++ src/api/manuals-overview-page/routes/manuals-overview-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * manuals-overview-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::manuals-overview-page.manuals-overview-page');
