b0fcc4b76bcb6b1a1eca0e06c400e9d59e1790d5
Seppe Alaerts <45093145+seppealaerts@users.noreply.github.com>
2025-01-03 11:45:45 +0100

    Add manual and manual overview page (#56)
    Co-authored-by: Seppe Alaerts <seppe.alaerts@craftzing.com>

---
+++ src/api/manuals-overview-page/services/manuals-overview-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * manuals-overview-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::manuals-overview-page.manuals-overview-page');
