232bb76b4ed8617397fb40bf39d3674fac74108a
Seppe Alaerts <seppe.alaerts@craftzing.com>
2026-05-07 15:41:42 +0200

    Clean up env files

--- config/database.js
+++ config/database.js
@@ -1,27 +1,13 @@
 module.exports = ({ env }) => {
-  const appEnv = env("APP_ENV");
-  const prefixByEnv = {
-    production: "DB",
-    development: "DEV_DB",
-    uat: "UAT_DB",
-  };
-
-  const prefix = prefixByEnv[appEnv];
-  if (!prefix) {
-    throw new Error(
-      `Invalid APP_ENV "${appEnv}". Expected one of: ${Object.keys(prefixByEnv).join(", ")}`
-    );
-  }
-
   return {
     connection: {
       client: "mysql",
       connection: {
-        host: env(`${prefix}_HOST`),
-        port: env.int(`${prefix}_PORT`),
-        database: env(`${prefix}_NAME`),
-        user: env(`${prefix}_USERNAME`),
-        password: env(`${prefix}_PASSWORD`),
+        host: env("DB_HOST"),
+        port: env.int("DB_PORT"),
+        database: env("DB_NAME"),
+        user: env("DB_USERNAME"),
+        password: env("DB_PASSWORD"),
       },
       debug: false,
     },
